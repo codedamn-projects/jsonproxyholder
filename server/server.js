929ae02626115402c3feb509d8a8c233dc12fc81
Mehul Mohan <mehulmpt@gmail.com>
2022-01-14 17:56:10 -0600

    server updates

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const fetch = require('node-fetch')
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
+
+app.use(cors())
 
 app.get('/', (req, res) => {
 	res.json({
