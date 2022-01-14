6c18a12a5da3c244fcabc25a30fef21fd2fa9996
Mehul Mohan <mehulmpt@gmail.com>
2022-01-14 16:56:31 -0600

    React app cleanup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,7 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.jsx</code> and save to reload!
-				</p>
-				<span className="App-link">Hello from codedamn :)</span>
-			</header>
-		</div>
-	)
+	return <h1>Hello World!</h1>
 }
 
 export default App
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,14 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import './index.css'
 
-ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
 // Learn more: https://www.snowpack.dev/#hot-module-replacement
